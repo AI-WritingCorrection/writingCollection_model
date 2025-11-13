2abacff22438d7022cdc6cc50405e80897176317
CeiSei <73943114+CeiSei@users.noreply.github.com>
2025-11-13 13:44:53 +0900

    Add files via upload

--- aiModel/utils/feedback.py
+++ aiModel/utils/feedback.py
@@ -1,3 +1,69 @@
+ERROR_FEEDBACK_2ND_CHILD = {
+
+    'TOO_BIG' : '글씨가 너무 커요...',
+    'TOO_BIG_NORMAL' : '  - 글씨를 조금 작게 써 볼까요?',
+    'TOO_BIG_HORIZONTAL' : '  - 글씨를 가로로 조금 짧게 써 볼까요?',
+    'TOO_BIG_VERTICAL' : '  - 글씨를 세로로 조금 짧게 써 볼까요?',
+    'TOO_SMALL' : '글씨가 너무 작아요...',
+    'TOO_SMALL_NORMAL' : '  - 글씨를 조금 크게 써 볼까요?',
+    'TOO_SMALL_HORIZONTAL' : '  - 글씨를 가로로 조금 길게 써 볼까요?',
+    'TOO_SMALL_VERTICAL' : '  - 글씨를 세로로 조금 길게 써 볼까요?'
+}
+
+ERROR_FEEDBACK_2ND_ADULT = {
+    
+    'TOO_BIG' : '글씨가 너무 커요...',
+    'TOO_BIG_NORMAL' : '  - 글씨를 조금 작게 써 주세요.',
+    'TOO_BIG_HORIZONTAL' : '  - 글씨가 가로로 조금 작게 써 주세요.',
+    'TOO_BIG_VERTICAL' : '  - 글씨를 세로로 조금 작게 써 주세요.',
+    'TOO_SMALL' : '글씨가 너무 작아요...',
+    'TOO_SMALL_NORMAL' : '  - 글씨를 조금 크게 써 주세요.',
+    'TOO_SMALL_HORIZONTAL' : '  - 글씨를 가로로 조금 크게 써 주세요.',
+    'TOO_SMALL_VERTICAL' : '  - 글씨를 세로로 조금 크게 써 주세요.'
+}
+
+ERROR_FEEDBACK_2ND_FOREIGN = {
+
+    'TOO_BIG' : 'The letter is too big.',
+    'TOO_BIG_NORMAL' : '  - Please write the letter smaller.',
+    'TOO_BIG_HORIZONTAL' : '  - Please write the letter smaller horizontally.',
+    'TOO_BIG_VERTICAL' : '  - Please write the letter smaller vertically.',
+    'TOO_SMALL' : 'The letter is too small.',
+    'TOO_SMALL_NORMAL' : '  - Please write the letter more bigger.',
+    'TOO_SMALL_HORIZONTAL' : '  - Please write the letter more stretched horizontally.',
+    'TOO_SMALL_VERTICAL' : '  - Please write the letter more stretched vertically.'
+}
+
+ERROR_FEEDBACK_3RD_CHILD = {
+
+    #~의 ~번째 획을 반대로 썼어요...
+    # LR -> 왼쪽에서 오른쪽으로 쓰는 것이 정답이나, 반대로 쓴 경우의 피드백.
+    'WRONG_STROKE' : '획의 방향이 잘못된 글자가 있어요.',
+    'WRONG_STROKE_REVERSE' : '  - 번째 획을 반대로 썼어요.',
+    'WRONG_STROKE_LR' : '    - 왼쪽에서 오른쪽으로 써볼까요?',
+    'WRONG_STROKE_RL' : '    - 오른쪽에서 왼쪽으로 써볼까요?',
+    'WRONG_STROKE_UD' : '    - 위에서 아래로 써볼까요?',
+    'WRONG_STROKE_DU' : '    - 아래에서 위로 써볼까요?'
+}
+
+ERROR_FEEDBACK_3RD_ADULT = {
+    'WRONG_STROKE' : '획의 방향이 잘못된 글자가 있어요.',
+    'WRONG_STROKE_REVERSE' : '  - 번째 획을 반대로 썼어요.',
+    'WRONG_STROKE_LR' : '    - 왼쪽에서 오른쪽으로 써주세요.',
+    'WRONG_STROKE_RL' : '    - 오른쪽에서 왼쪽으로 써주세요.',
+    'WRONG_STROKE_UD' : '    - 위에서 아래로 써주세요.',
+    'WRONG_STROKE_DU' : '    - 아래에서 위로 써주세요.'
+}
+
+ERROR_FEEDBACK_3RD_FOREIGN = {
+    'WRONG_STROKE' : 'There is a letter with the wrong direction of the stroke.',
+    'WRONG_STROKE_REVERSE' : '  - stroke is wrong.',
+    'WRONG_STROKE_LR' : '    - Please write your stroke from left to right.',
+    'WRONG_STROKE_RL' : '    - Please write your stroke from right to left.',
+    'WRONG_STROKE_UD' : '    - Please write your stroke from top to bottom.',
+    'WRONG_STROKE_DU' : '    - Please write your stroke from bottom to top.'
+}
+
 ERROR_FEEDBACK_4TH_CHILD = {
 
     'FIRST_CELL' : "첫 번째 글자",
